c72e0843e63ac1bac794c025c0f257c2421f4888
maryam aalizadeh <maalizadeh7394@gmail.com>
2025-03-14 16:49:28 +0330

    new home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-900 mb-4">
+        Welcome to CODEENGINEER ADMIN PANEL
+      </h1>
+      <Link
+        href="/signin"
+        className="bg-blue-500 text-white p-3 rounded-lg shadow-md hover:bg-blue-600 transition duration-300 text-center"
+      >
+        Get Started
+      </Link>
+    </div>
+  );
 }
